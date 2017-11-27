d9662d8a6701f6aca2821a66dbb4b578b5004bbb
Juan <Juan@Juan-PC>
2017-11-27 16:25:50 +0100

    Clase PersoasCole finalizada

--- src/colexio/PersoasCole.java
+++ src/colexio/PersoasCole.java
@@ -7,33 +7,68 @@ public class PersoasCole {
 
     private String nome, direccion;
 
+    /**
+     * Constructor por defecto.
+     */
     public PersoasCole() {
     }
 
+    /**
+     * Constructor con parametros.
+     *
+     * @param nome
+     * @param direccion
+     */
     public PersoasCole(String nome,String direccion) {
-        this.nome=nome;
-        this.direccion=direccion;
+        this.nome = nome;
+        this.direccion = direccion;
     }
 
+    /**
+     * Metodo getNome que retorna el valor de nome.
+     *
+     * @return
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Metodo setNome que la da un valor de tipo String a nome.
+     *
+     * @param nome
+     */
     public void setNome(String nome) {
-        this.nome=nome;
+        this.nome = nome;
     }
+
+    /**
+     * Metodo getDireccion que retorna el valor de direccion.
+     *
+     * @return
+     */
 
     public String getDireccion() {
         return direccion;
     }
 
+    /**
+     * Metodo setDireccion que asigna un valor de tipo String a direccion.
+     *
+     * @param direccion
+     */
     public void setDireccion(String direccion) {
-        this.direccion=direccion;
+        this.direccion = direccion;
     }
 
+    /**
+     * Metodo toString que retorna un valor de tipo String.
+     *
+     * @return
+     */
     @Override
     public String toString() {
-        return ("Nome :"+nome+"\nDireccion:"+direccion);
+        return ("Nome :" + nome + "\nDireccion:" + direccion);
     }
 
 }
